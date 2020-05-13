d7bcf8c1a97399b2170f0508866910547d9d46b6
robertosc <robert.s.c30@gmail.com>
2020-05-13 00:43:31 -0600

    colas terminado

--- Laboratorio1/cola.cpp
+++ Laboratorio1/cola.cpp
@@ -57,18 +57,15 @@ public:
 
 	void agregar(T valor){
 		Nodo<T> *nuevo = new Nodo<T>(valor);
-		Nodo<T> *start = inicio;
-		Nodo<T> *temp = final; //Debemos llevar cuenta del final, el incio se mantiene
-		if (start == nullptr){
+		if (inicio == nullptr){
 
 			inicio = nuevo;
 			final = nuevo;
 			//cout << final->obtenerValor();
 		}
 		else{
-			//cout << "Hola";
 			final->establecerSiguiente(nuevo); //incialmente final es el anterior al nuevo
-			temp = nuevo; //Queremos que final, guardado en temp, apunte al nuevo
+			final = nuevo; //Queremos que final, guardado en temp, apunte al nuevo
 			//cout << final->obtenerValor();
 		}
 	}
@@ -82,12 +79,75 @@ public:
 			temp = temp->obtenerSiguiente();
 		}
 	}
+
+	void eliminar(){
+		if (inicio == nullptr){
+			return;
+		}
+		else if(inicio == final){
+			//cout << "Hola";
+			Nodo<T> *tmp_i = inicio;
+			Nodo<T> *tmp_f = final;
+			inicio = nullptr;
+			final = nullptr;
+		}
+		else{
+			Nodo<T> *tmp = inicio;
+			inicio = inicio->obtenerSiguiente();
+			tmp->establecerSiguiente(nullptr);
+			delete tmp;
+			return;
+		}
+	}
+
+	int longitud(){
+		Nodo<T> *tmp = inicio;
+		int contador = 0;
+		if (tmp == nullptr){
+			return 0;
+		}
+		while (tmp != nullptr){
+			contador +=1;
+			tmp = tmp->obtenerSiguiente();
+		}
+		return contador;
+	}
+
+	bool estaVacio(){
+		if (longitud() == 0){
+			return 1;
+		}
+		else{
+			return 0;
+		}
+	}
+
+	void reiniciar(){
+		int i = 0;
+		//Nodo<T> *tmp = inicio;
+		if (estaVacio()){
+			//cout << "Hola";
+			return;
+		}
+		else{
+			for (i = 0; i<=longitud()+1; i++){
+				//cout << "Hola";
+				eliminar();
+			}
+		}
+	}
+
+
 };
 
 int main(){
 	Cola<int> cola;
-	cola.agregar(1);
 	cola.agregar(2);
 	cola.agregar(3);
+	cola.agregar(1);
+	//cola.eliminar();
+	cola.reiniciar();
+	cola.agregar(10);
 	cola.mostrar();
+	//cout << cola.longitud();
 }
